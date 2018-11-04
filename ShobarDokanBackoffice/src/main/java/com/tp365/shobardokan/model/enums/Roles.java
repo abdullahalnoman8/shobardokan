9c73ff0ed928c6fed65d3ea80b93fb3d3e10d472
aabdullahalmasum <aabdullahalmasum@gmail.com>
2018-11-04 12:49:48 +0600

    Separate enum class added

---
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/enums/Roles.java
@@ -0,0 +1,8 @@
+package com.tp365.shobardokan.model.enums;
+
+/**
+ * Created by Masum on 11/1/2018.
+ */
+public enum  Roles {
+    ADMIN, USER, TRAVELER
+}
---
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/enums/Status.java
@@ -0,0 +1,8 @@
+package com.tp365.shobardokan.model.enums;
+
+/**
+ * Created by Masum on 11/4/2018.
+ */
+public enum  Status {
+    WAITING, FINISHED, ON_PROCESS, ACCEPTED
+}

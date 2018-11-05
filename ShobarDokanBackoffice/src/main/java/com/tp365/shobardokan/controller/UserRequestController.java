d6362b71c916fb2d6af5dee82c89ae6a42924a96
sayed.mahmud <sayedmahmudraihan@gmail.com>
2018-11-05 11:27:21 +0600

    validation added and user request status updated list

--- ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/controller/UserRequestController.java
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/controller/UserRequestController.java
@@ -6,13 +6,14 @@ import com.tp365.shobardokan.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -25,25 +26,28 @@ public class UserRequestController {
     UserRequestService userRequestService;
 
     @RequestMapping(value = "/list")
-    public String getAllRequestList(Model model, Principal principal) {
-        String userName = principal.getName();
-        int userId = userService.findUserByUserName(userName).getId();
-        List<UserRequest> requestedProductList = userRequestService.findAllRequestedProductsByUserId(userId);
+    public String getAllRequestList(Model model) {
+        List<UserRequest> requestedProductList = userRequestService.findAllRequestedProducts();
         model.addAttribute("requestedProductList", requestedProductList);
         return "product_request/product_request_list";
     }
 
     @RequestMapping("/add")
-    public String add(@ModelAttribute("add") UserRequest userRequest,
+    public String add(@ModelAttribute @Valid UserRequest userRequest, BindingResult bindingResult,
                       HttpServletRequest request, final RedirectAttributes redirectAttributes){
         if (request.getMethod().equals(RequestMethod.POST.toString())) {
-            if (userRequestService.add(userRequest)) {
-                redirectAttributes.addFlashAttribute("success", "Request received Successfully");
-                return "redirect:list";
+            if (bindingResult.hasErrors()) {
+                return "product_request/product_request_add";
             } else {
-                redirectAttributes.addFlashAttribute("error", "Request received Failed");
-                return "redirect:/requestProduct/add";
+                if (userRequestService.add(userRequest)) {
+                    redirectAttributes.addFlashAttribute("success", "Request received Successfully");
+                    return "redirect:list";
+                } else {
+                    redirectAttributes.addFlashAttribute("error", "Request received Failed");
+                    return "redirect:/requestProduct/add";
+                }
             }
+
         }
         return "product_request/product_request_add";
     }
--- ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/UserRequest.java
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/UserRequest.java
@@ -2,6 +2,7 @@ package com.tp365.shobardokan.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
@@ -9,14 +10,26 @@ public class UserRequest {
 
     private Integer id;
     private User user;
+    @NotNull
+    @Size(min=2, max=300)
+//    @Pattern(regexp="^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/|www\\.)[a-z0-9]+([\\-\\" +
+//            ".]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$", message = "URL should be valid")
+    @Pattern(regexp = "^((http|https)://www.(a-z|0-9).(a-z|0-9)//(a-z|0-9)-)?.+\\..+$", message = "URL Should be valid")
     private String productUrl;
     private Status status;
-    private Integer quantity;
+    @NotNull
+    @Digits(integer=2, fraction=0, message = "Quantity can only be 2 digits long or less")
+    @Min(value = 1, message = "Quantity should not be less than 1")
+    @Max(value = 10, message = "Quantity should not be greater than 10")
+    private int quantity;
     private Date createdAt;
     private Date updatedAt;
+    @NotNull
+    @Size(min=5, max=200,  message = "Comments must be between 5 and 200 Characters")
+    @Pattern(regexp = "^[.0-9A-Za-z\\s!_:',@#*\"%+=\\$&()/-]+$", message = "Comments should be valid")
     private String comments;
 
-    public enum Status{
+    public enum Status {
         OPEN,CLOSED
     }
 }

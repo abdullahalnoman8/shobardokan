8473b3690f309005ccb40597a1a089991acf8632
Shahed Bin Satter <shahed.satter@datasoft-bd.com>
2018-11-08 17:16:39 +0600

    New Constructor for easy initialization

--- ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/UsersRoles.java
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/UsersRoles.java
@@ -1,14 +1,24 @@
 package com.tp365.shobardokan.model;
 
+import com.tp365.shobardokan.model.enums.Roles;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 public class UsersRoles {
     private Integer id;
     private User user;
     private Role role;
     private Date createdAt;
     private Date deletedAt;
+    
+    public UsersRoles(Integer id, String role) {
+    	this.user = new User();
+    	this.role = new Role();
+    	user.setId(id);
+    	this.role.setRole(Roles.valueOf(role));
+    }
 }

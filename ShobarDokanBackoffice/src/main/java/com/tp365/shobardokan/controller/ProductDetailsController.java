282ded827195bb7a610dfc5ebaab93d657683c20
aabdullahalmasum <aabdullahalmasum@gmail.com>
2018-11-12 16:14:29 +0600

    javax.validation implemented

--- ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/controller/ProductDetailsController.java
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/controller/ProductDetailsController.java
@@ -1,6 +1,7 @@
 package com.tp365.shobardokan.controller;
 
 import com.tp365.shobardokan.model.ProductDetails;
+import com.tp365.shobardokan.model.UserRequest;
 import com.tp365.shobardokan.service.CategoryService;
 import com.tp365.shobardokan.service.ProductDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,14 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.validation.Valid;
 
 /**
  * Created by Masum on 10/25/2018.
@@ -31,23 +33,36 @@ public class ProductDetailsController {
     @Autowired
     private Environment env;
 
-    @RequestMapping(value = "/add")
-    public String add(@ModelAttribute("productDetails") ProductDetails productDetails, HttpServletRequest request, Model model) {
-        if (request.getMethod().equals(RequestMethod.POST.toString())) {
-            if (productDetailsService.add(productDetails)) {
-                model.addAttribute("success", "Product data updated Successfully");
-            } else {
-                model.addAttribute("error", "Product data updated Unsuccessfully");
-            }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(@ModelAttribute("productDetails") ProductDetails productDetails, Model model) {
+        if (productDetailsService.add(productDetails)) {
+            model.addAttribute("success", "Product data updated Successfully");
+        } else {
+            model.addAttribute("error", "Product data updated Unsuccessfully");
         }
         model.addAttribute("productDetailsList", productDetailsService.findAll());
         return "productDetails/add";
     }
 
-    @RequestMapping("/editUncheckedProduct")
-    public String edit(Model model, @ModelAttribute("productDetails") ProductDetails productDetails,
-                       HttpServletRequest request, final RedirectAttributes redirectAttributes) throws IOException {
-        if (request.getMethod().equals(RequestMethod.POST.toString())) {
+
+    @RequestMapping(value = "/editUncheckedProduct", method = RequestMethod.GET)
+    public String editUncheckedProduct(HttpServletRequest request, Model model) {
+        UserRequest userRequest = productDetailsService.requestedUncheckedProductById(Integer.valueOf(request.getParameter("id")));
+        ProductDetails newProductDetails = new ProductDetails();
+        newProductDetails.setUserRequest(userRequest);
+        newProductDetails.setUrl(userRequest.getProductUrl());
+        model.addAttribute("productDetails", newProductDetails);
+        model.addAttribute("productCategory", categoryService.getProductCategoryList());
+        return "productDetails/edit_unchecked_product";
+    }
+
+    @RequestMapping(value = "/updateUncheckedProduct", method = RequestMethod.POST)
+    public String updateUncheckedProduct(Model model, @ModelAttribute @Valid ProductDetails productDetails, BindingResult bindingResult,
+                                         final RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("productCategory", categoryService.getProductCategoryList());
+            return "productDetails/edit_unchecked_product";
+        } else {
             if (productDetailsService.add(productDetails)) {
                 redirectAttributes.addFlashAttribute("success", env.getProperty("msg.update.successfull"));
             } else {
@@ -55,15 +70,24 @@ public class ProductDetailsController {
             }
             return "redirect:unCheckedProductDetailsList";
         }
-        model.addAttribute("requestedProductInfo", productDetailsService.requestedUncheckedProductById(productDetails.getId()));
-        model.addAttribute("productCategory", categoryService.getProductCategoryList());
-        return "productDetails/edit_unchecked_product";
     }
 
-    @RequestMapping("/editCheckedProduct")
-    public String editCheckedProduct(Model model, @ModelAttribute("productDetails") ProductDetails productDetails,
-                                     HttpServletRequest request, final RedirectAttributes redirectAttributes) throws IOException {
-        if (request.getMethod().equals(RequestMethod.POST.toString())) {
+    @RequestMapping(value = "/editCheckedProduct", method = RequestMethod.GET)
+    public String editCheckedProduct(HttpServletRequest request, Model model) {
+        ProductDetails newProductDetails;
+        newProductDetails = productDetailsService.findById(Integer.valueOf(request.getParameter("id")));
+        model.addAttribute("productDetails", newProductDetails);
+        model.addAttribute("productCategory", categoryService.getProductCategoryList());
+        return "productDetails/edit_checked_product";
+    }
+
+    @RequestMapping(value = "/updateCheckedProduct", method = RequestMethod.POST)
+    public String updateCheckedProduct(Model model, @ModelAttribute @Valid ProductDetails productDetails, BindingResult bindingResult,
+                                       final RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("productCategory", categoryService.getProductCategoryList());
+            return "productDetails/edit_checked_product";
+        } else {
             if (productDetailsService.update(productDetails)) {
                 redirectAttributes.addFlashAttribute("success", env.getProperty("msg.update.successfull"));
             } else {
@@ -71,9 +95,6 @@ public class ProductDetailsController {
             }
             return "redirect:checkedProductDetailsList";
         }
-        model.addAttribute("productDetails", productDetailsService.findById(productDetails.getId()));
-        model.addAttribute("productCategory", categoryService.getProductCategoryList());
-        return "productDetails/edit_checked_product_details";
     }
 
     @RequestMapping("/unCheckedProductDetailsList")
--- ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/ProductDetails.java
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/ProductDetails.java
@@ -4,23 +4,56 @@ import com.tp365.shobardokan.model.enums.Status;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
 public class ProductDetails {
+
     private Integer id;
+
+    @NotNull
     private UserRequest userRequest;
+
     private Category category;
+
+    @NotNull
+    @Size(min = 2)
+    @Pattern(regexp = "^((http|https)://www.(a-z|0-9).(a-z|0-9)//(a-z|0-9)-)?.+\\..+$", message = "URL Should be valid")
     private String url;
+
+    @Size(min = 5, message = "Description should be more 5 Characters")
+    @Pattern(regexp = "^[.0-9A-Za-z\\s!_:',@#*\"%+=\\$&()/-]+$", message = "Description should be valid")
     private String productDescription;
-    private Double unitPrice;
-    private Double deliveryFee;
+
+    @Digits(integer = 6, fraction = 2, message = "Delivery Fee should be more 0")
+    private BigDecimal deliveryFee;
+
+    @NotNull
+    @Valid
+    @Enumerated(EnumType.STRING)
     private Status status;
-    private Double costPrice;
+
+    @NotNull
+    @Digits(integer = 4, fraction = 2, message = "Quantity can only be 2 digits long or less")
+    private BigDecimal unitPrice;
+
+    @NotNull
+    @Digits(integer = 6, fraction = 2, message = "Cost Price should be more 0")
+    private BigDecimal costPrice;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Future(message = "Estimated Delivery Date Should be Future Date")
     private Date estimatedDeliveryDate;
+
     private String comments;
+
     private User user;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date priceValidTill;
 }

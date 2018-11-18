33613b346cdd53440a30971a6f906362159bdf76
aabdullahalmasum <aabdullahalmasum@gmail.com>
2018-11-18 11:29:04 +0600

    Model updated with enum

--- ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/UserRequest.java
+++ ShobarDokanBackoffice/src/main/java/com/tp365/shobardokan/model/UserRequest.java
@@ -3,6 +3,7 @@ package com.tp365.shobardokan.model;
 import lombok.Data;
 
 import javax.validation.constraints.*;
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
@@ -12,16 +13,12 @@ public class UserRequest {
     private User user;
     @NotNull
     @Size(min=2, max=300)
-//    @Pattern(regexp="^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/|www\\.)[a-z0-9]+([\\-\\" +
-//            ".]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$", message = "URL should be valid")
     @Pattern(regexp = "^((http|https)://www.(a-z|0-9).(a-z|0-9)//(a-z|0-9)-)?.+\\..+$", message = "URL Should be valid")
     private String productUrl;
     private Status status;
     @NotNull
-    @Digits(integer=2, fraction=0, message = "Quantity can only be 2 digits long or less")
-    @Min(value = 1, message = "Quantity should not be less than 1")
-    @Max(value = 10, message = "Quantity should not be greater than 10")
-    private int quantity;
+    @Digits(integer=4, fraction=2, message = "Quantity can only be 2 digits long or less")
+    private BigDecimal quantity;
     private Date createdAt;
     private Date updatedAt;
     @NotNull
